e3cb3ca67ca9bc88aa0f78cc25f3565bda6eab64
Marcin Skrzekut <marcin.skrzekut@gmail.com>
2019-11-23 15:33:09 +0100

    project setup

---
+++ src/app/material.ts
@@ -0,0 +1,9 @@
+import { MatToolbarModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [MatToolbarModule],
+    exports: [MatToolbarModule]
+})
+
+export class MaterialModule {}
