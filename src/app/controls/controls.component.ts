263fb12c65ba0c592463b3780df83c845820b2af
Marcin Skrzekut <marcin.skrzekut@gmail.com>
2019-12-23 15:14:38 +0100

    finish controls

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-controls',
+  selector: "app-controls",
   template: `
-    <mat-toolbar color="primary" class='mat-elevation-z1'>
-      <div class='controls-container'>
-        <mat-card></mat-card>
+    <mat-toolbar color="primary" class="mat-elevation-z1">
+      <div class="controls-container">
+        <mat-card>
+          <mat-card-title>Panel kontrolny</mat-card-title>
+          <mat-card-content>
+            <div class='input-container'>
+              <input matInput placeholder="Szukana fraza" type="search"/>
+            </div>
+            <div class="buttons-container">
+              <button mat-stroked-button color="primary" class="top-row">
+                Kompletny proces ETL
+              </button>
+              <button mat-stroked-button color="primary" class="bottom-row-extract">Extract</button>
+              <button mat-stroked-button color="primary" class="bottom-row-transfer">Transform</button>
+              <button mat-stroked-button color="primary" class="bottom-row-load">Load</button>
+            </div>
+          </mat-card-content>
+        </mat-card>
       </div>
     </mat-toolbar>
   `,
-  styleUrls: ['./controls.component.scss']
+  styleUrls: ["./controls.component.scss"]
 })
-export class ControlsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ControlsComponent {}
--- src/app/material.ts
+++ src/app/material.ts
@@ -2,12 +2,25 @@ import {
   MatToolbarModule,
   MatButtonModule,
   MatIconModule,
-  MatCardModule
+  MatCardModule,
+  MatInputModule
 } from "@angular/material";
 import { NgModule } from "@angular/core";
 
 @NgModule({
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule],
-  exports: [MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule]
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule
+  ]
 })
 export class MaterialModule {}
